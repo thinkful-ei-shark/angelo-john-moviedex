6f4d070bc499b187d896105b63283b072afada3b
John Wellbrock <wellbrock23john@gmail.com>
2020-10-20 21:37:55 -0600

    fixed filter issues

--- server.js
+++ server.js
@@ -32,15 +32,15 @@ function handleGetMovies(req, res) {
 //genre country avg_vote
     if(req.query.genre) {
         response = response.filter(movie =>
-            movie.genre.toLowerCase() === req.query.genre.toLowerCase())
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
     }
     if(req.query.country) {
         response = response.filter(movie =>
-            movie.country.toLowerCase() === req.query.country.toLowerCase())
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
     }
     if(req.query.avg_vote) {
         response = response.filter(movie => 
-            parseInt(movie.avg_vote) >= parseInt(req.query.avg_vote))
+            Number(movie.avg_vote) >= Number(req.query.avg_vote))
     }
   res.json(response);
 
