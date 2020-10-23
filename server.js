117e7e062d3b25f6d2f5926fcf3e38b438033678
John Wellbrock <wellbrock23john@gmail.com>
2020-10-23 12:29:06 -0600

    deployment ready

--- server.js
+++ server.js
@@ -5,51 +5,59 @@ const cors = require("cors");
 const helmet = require('helmet');
 const Movies = require('./movies-data-small.json');
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
 
-app.use(morgan("common"));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
 
-const apiToken = process.env.API_TOKEN
-const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
 
-//   console.log("validate bearer token middleware");
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
 
-  if(!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({ error: 'Unauthorized request'})
-  }
- 
-  //move to the next middleware
-  next();
+    //move to the next middleware
+    next();
 });
 
 function handleGetMovies(req, res) {
     let response = Movies;
-//genre country avg_vote
-    if(req.query.genre) {
+    //genre country avg_vote
+    if (req.query.genre) {
         response = response.filter(movie =>
             movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
     }
-    if(req.query.country) {
+    if (req.query.country) {
         response = response.filter(movie =>
             movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
     }
-    if(req.query.avg_vote) {
-        response = response.filter(movie => 
+    if (req.query.avg_vote) {
+        response = response.filter(movie =>
             Number(movie.avg_vote) >= Number(req.query.avg_vote))
     }
-  res.json(response);
+    res.json(response);
 
 }
 
 app.get("/movies", handleGetMovies);
 
-const PORT = 8000;
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log('Your server is listening on port 8000!')
-});
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
